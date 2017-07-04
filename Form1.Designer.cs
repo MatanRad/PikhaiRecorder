f6c6d6b59c86030118da941d2bd9f9ab05382070
MatanRad <MatanRad@users.noreply.github.com>
2017-07-04 17:00:39 +0300

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Pikhai_Recorder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RecordButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FreqTrack = new System.Windows.Forms.TrackBar();
+            this.AmpTrack = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.FreqTrack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AmpTrack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RecordButton
+            // 
+            this.RecordButton.Location = new System.Drawing.Point(13, 13);
+            this.RecordButton.Name = "RecordButton";
+            this.RecordButton.Size = new System.Drawing.Size(161, 23);
+            this.RecordButton.TabIndex = 0;
+            this.RecordButton.Text = "Record";
+            this.RecordButton.UseVisualStyleBackColor = true;
+            this.RecordButton.Click += new System.EventHandler(this.RecordButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Enabled = false;
+            this.SaveButton.Location = new System.Drawing.Point(180, 13);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(164, 23);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(180, 42);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(164, 23);
+            this.LoadButton.TabIndex = 2;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Enabled = false;
+            this.PlayButton.Location = new System.Drawing.Point(10, 42);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(164, 23);
+            this.PlayButton.TabIndex = 3;
+            this.PlayButton.Text = "Play";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Frequency";
+            // 
+            // FreqTrack
+            // 
+            this.FreqTrack.Location = new System.Drawing.Point(16, 89);
+            this.FreqTrack.Maximum = 22050;
+            this.FreqTrack.Minimum = 20;
+            this.FreqTrack.Name = "FreqTrack";
+            this.FreqTrack.Size = new System.Drawing.Size(328, 45);
+            this.FreqTrack.TabIndex = 5;
+            this.FreqTrack.TickFrequency = 0;
+            this.FreqTrack.Value = 2142;
+            this.FreqTrack.Scroll += new System.EventHandler(this.FreqTrack_Scroll);
+            // 
+            // AmpTrack
+            // 
+            this.AmpTrack.Location = new System.Drawing.Point(16, 143);
+            this.AmpTrack.Maximum = 5000;
+            this.AmpTrack.Minimum = 50;
+            this.AmpTrack.Name = "AmpTrack";
+            this.AmpTrack.Size = new System.Drawing.Size(328, 45);
+            this.AmpTrack.TabIndex = 6;
+            this.AmpTrack.TickFrequency = 0;
+            this.AmpTrack.Value = 1168;
+            this.AmpTrack.Scroll += new System.EventHandler(this.AmpTrack_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Amplitude";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 184);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.AmpTrack);
+            this.Controls.Add(this.FreqTrack);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PlayButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.RecordButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Name = "MainForm";
+            this.Text = "Pikhai Recorder - Rawaii Kawaii";
+            ((System.ComponentModel.ISupportInitialize)(this.FreqTrack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AmpTrack)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RecordButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar FreqTrack;
+        private System.Windows.Forms.TrackBar AmpTrack;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Pikhai_Recorder
+{
+    public partial class MainForm : Form
+    {
+        [Serializable]
+        private class Recording
+        {
+            [Serializable]
+            public class Note
+            {
+                public TimeSpan Pressed;
+                public TimeSpan Released;
+            }
+
+            string filepath;
+            List<Note> Notes = new List<Note>();
+
+            public double Frequency = 100;
+            public double Amplitude = 100;
+
+            private void PlayForTime(TimeSpan span)
+            {
+                DirectSoundOut output = new DirectSoundOut();
+                int streamsize = (int)Math.Ceiling(span.TotalSeconds * 44100);
+
+                MemoryStream mstream = new MemoryStream();
+
+                WaveFileWriter writer = new WaveFileWriter(mstream, new WaveFormat(44100, 1));
+                for (int i = 0; i < streamsize; i++) writer.WriteSample((float)Amplitude * (float)Math.Sin((2 * Math.PI * i * Frequency) / 44100));
+
+                writer.Flush();
+                
+                mstream.Position = 0;
+
+                WaveFileReader reader = new WaveFileReader(mstream);
+                WaveChannel32 wav = new WaveChannel32(reader);
+                wav.PadWithZeroes = false;
+
+                output.Init(wav);
+                output.Play();
+                while (output.PlaybackState == PlaybackState.Playing);
+
+                reader.Close();
+            }
+
+            public void Play()
+            {
+                if (Notes.Count == 0) return;
+                int i = 0;
+
+                DateTime start = DateTime.Now;
+
+                while(i < Notes.Count)
+                {
+                    if(start.Add(Notes[i].Pressed)<=DateTime.Now)
+                    {
+                        PlayForTime(Notes[i].Released - Notes[i].Pressed);
+                        i++;
+                    }
+                }
+            }
+
+            private DateTime StartDate;
+
+            public Recording()
+            {
+                StartDate = DateTime.Now;
+            }
+
+            public void Tap()
+            {
+                Note n = new Note();
+                n.Pressed = DateTime.Now-StartDate;
+                Notes.Add(n);
+            }
+
+            public void Release()
+            {
+                if (Notes.Count == 0) return;
+                Notes[Notes.Count - 1].Released = DateTime.Now - StartDate;
+            }
+        }
+
+        bool IsRecording
+        {
+            get
+            {
+                return current != null;
+            }
+        }
+
+        const string RecordStr = "Record";
+        const string StopRecordStr = "Stop";
+        const Keys TapKey = Keys.ShiftKey;
+
+        Recording last;
+        Recording current = null;
+
+        
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.KeyDown += MainForm_KeyDown;
+            this.KeyUp += MainForm_KeyUp; ;
+        }
+
+        private void MainForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == TapKey && IsRecording && sender == this)
+            {
+                current.Release();
+                RecordButton.Enabled = true;
+                pressedup = true;
+            }
+        }
+
+        bool pressedup = true;
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == TapKey && IsRecording && sender == this && pressedup)
+            {
+                current.Tap();
+                RecordButton.Enabled = false;
+                pressedup = false;
+            }
+        }
+
+        private void RecordButton_Click(object sender, EventArgs e)
+        {
+            if(!IsRecording)
+            {
+                current = new Recording();
+                current.Frequency = FreqTrack.Value;
+                current.Amplitude = AmpTrack.Value;
+                RecordButton.Text = StopRecordStr;
+
+                PlayButton.Enabled = false;
+                SaveButton.Enabled = false;
+                LoadButton.Enabled = false;
+                FreqTrack.Enabled = false;
+                AmpTrack.Enabled = false;
+            }
+            else
+            {
+                last = current;
+                current = null;
+                RecordButton.Text = RecordStr;
+
+                PlayButton.Enabled = true;
+                SaveButton.Enabled = true;
+                LoadButton.Enabled = true;
+                FreqTrack.Enabled = true;
+                AmpTrack.Enabled = true;
+            }
+        }
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            if(last!=null)
+            {
+                last.Play();
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog d = new SaveFileDialog();
+            d.InitialDirectory = System.Environment.CurrentDirectory;
+            d.Filter = "Pikh Cringe File (*.pikh)|*.pikh";
+            d.DefaultExt = "pikh";
+            d.AddExtension = true;
+            var res = d.ShowDialog();
+            if(res == DialogResult.OK)
+            {
+                FileStream str = new FileStream(d.FileName, FileMode.Create);
+                BinaryFormatter f = new BinaryFormatter();
+                f.Serialize(str, last);
+                str.Close();
+            }
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog d = new OpenFileDialog();
+            d.InitialDirectory = System.Environment.CurrentDirectory;
+            d.Filter = "Pikh Cringe File (*.pikh)|*.pikh";
+            d.DefaultExt = "pikh";
+            d.AddExtension = true;
+            var res = d.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                FileStream str = new FileStream(d.FileName, FileMode.Open);
+                BinaryFormatter f = new BinaryFormatter();
+                last = (Recording)f.Deserialize(str);
+                SaveButton.Enabled = true;
+                PlayButton.Enabled = true;
+
+                str.Close();
+
+                AmpTrack.Value = (int)last.Amplitude;
+                FreqTrack.Value = (int)last.Frequency;
+                Debug.WriteLine("Freq: " + FreqTrack.Value + " Amp: " + AmpTrack.Value);
+            }
+        }
+
+        private void FreqTrack_Scroll(object sender, EventArgs e)
+        {
+        }
+
+        private void AmpTrack_Scroll(object sender, EventArgs e)
+        {
+        }
+    }
+}
